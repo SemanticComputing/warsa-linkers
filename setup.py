cd53a4eb07e33f53c147f65a2863afd5159056c2
Mikko Koho <mikko.koho@aalto.fi>
2017-07-14 11:41:25 +0300

    Make package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="warsa_linkers",
+    version="0.1.0",
+    author="Erkki Heino",
+    description="WarSampo entity linking",
+    license="MIT",
+    keywords="rdf",
+    url="https://github.com/SemanticComputing/warsa-linkers",
+    long_description=read('README.md'),
+    packages=['warsa_linkers'],
+    install_requires=[
+        'rdflib >= 4.2.0',
+        'requests >= 2.7.0'
+    ],
+)
