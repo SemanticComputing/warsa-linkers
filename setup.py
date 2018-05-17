6ed7ca255d993bd8db487e4007ef54ab7bee2a09
Mikko Koho <mikko.koho@aalto.fi>
2018-05-17 12:40:08 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name="warsa_linkers",
-    version="0.2.3",
+    version="0.3.0",
     author="Erkki Heino, Mikko Koho",
     description="WarSampo entity linking",
     license="MIT",
